c1d7f36c511eeed3ed033d0cffa62204f7ad8bbd
Aleksandr <sasha.glagol.95@yandex.com>
2022-08-21 19:07:10 +0300

    finish

--- src/Components/Costs/Costs.js
+++ src/Components/Costs/Costs.js
@@ -3,7 +3,7 @@ import Card from '../UI/Card';
 import CostsFilter from './CostsFilter';
 import React, { useState } from 'react';
 import CostList from './CostList';
-
+import CostsDiagram from './CostsDiagram';
 
 
 function Costs(props) {
@@ -26,6 +26,7 @@ function Costs(props) {
     <div>
       <Card className='costs'>
         <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
+        <CostsDiagram costs={filteredCosts} />
         <CostList costs={filteredCosts} />
       </Card>
     </div>
--- src/Components/Diagram/Diagram.jsx
+++ src/Components/Diagram/Diagram.jsx
@@ -1,14 +1,18 @@
 import DiagramBar from "./DiagramBar";
-import './Diagram.css';
+import "./Diagram.css";
 
 const Diagram = (props) => {
+  const dataSetsValues = props.dataSets.map((dataSet) => dataSet.value);
+
+  const maxMonthCosts = Math.max(...dataSetsValues);
+
   return (
     <div className="diagram">
       {props.dataSets.map((dataSet) => (
         <DiagramBar
           key={dataSet.label}
           value={dataSet.value}
-          maxValue={null}
+          maxValue={maxMonthCosts}
           label={dataSet.label}
         />
       ))}
